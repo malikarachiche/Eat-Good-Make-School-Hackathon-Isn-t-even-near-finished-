4134308d290992c8772770f98c723d03c1e2a476
Malik Arachiche <arachich@msu.edu>
2018-07-16 15:13:35 -0700

    add xcode project

---
+++ Eat GoodTests/Eat_GoodTests.swift
@@ -0,0 +1,36 @@
+//
+//  Eat_GoodTests.swift
+//  Eat GoodTests
+//
+//  Created by Malik Arachiche on 7/16/18.
+//  Copyright © 2018 Malik Arachiche. All rights reserved.
+//
+
+import XCTest
+//@testable import Eat_Good
+
+class Eat_GoodTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
