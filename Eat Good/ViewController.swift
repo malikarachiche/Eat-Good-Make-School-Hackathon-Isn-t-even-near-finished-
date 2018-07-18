9d7c1137842510f626cab605699d94e9b8a81567
Leith Reardon <leithreardon@gmail.com>
2018-07-18 11:28:14 -0700

    merge "origin/malik" into Leith-Eat-Good-Branch

--- Eat Good/ViewController.swift
+++ Eat Good/ViewController.swift
@@ -12,16 +12,30 @@ import Alamofire
 import AlamofireImage
 import AlamofireNetworkActivityIndicator
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var navigationBar: UINavigationItem!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var cookingButton: UIButton!
+    
+    
+    var testData = ["Malik", "Aktar", "Leith", "Ekow", "Owais", "Tushar", "Nathan"]
+    
+    var filteredData = [String]()
+    
+    var isSearching = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+        
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
