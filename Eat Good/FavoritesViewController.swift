b79bebf009a9ede4e092ea29233795aa06950e70
Leith Reardon <leithreardon@gmail.com>
2018-07-19 15:10:50 -0700

    Revert "Merge remote-tracking branch 'origin/AZ-Branch' into Leith-Eat-Good-Branch"
    This reverts commit 7fd108770e9e273f6a9e505621c17efebeac3776.

--- Eat Good/FavoritesViewController.swift
+++ Eat Good/FavoritesViewController.swift
@@ -9,31 +9,7 @@
 import Foundation
 import UIKit
 
-class FavoritesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 1
-        
-    }
-    
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
-        
-        cell.textLabel?.text = "Chicken Curry"
-        
-        return cell
-        
-        
-    }
-    
-    
-    
-    
+class FavoritesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- Eat Good/MostViewedViewController.swift
+++ Eat Good/MostViewedViewController.swift
@@ -8,25 +8,11 @@
 
 import Foundation
 import UIKit
-import WebKit
 
 class MostViewedViewController: UIViewController {
-    
-    
-    @IBOutlet weak var yelpWebView: WKWebView!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let url = URL(string: "https://www.yelp.com/")
-        
-        let request = URLRequest(url: url!)
-        
-    yelpWebView.load(request)
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
